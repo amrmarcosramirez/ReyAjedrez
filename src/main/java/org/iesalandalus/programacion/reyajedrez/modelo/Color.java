df494d5028ff5855aeaa284ce898f5a2291643e4
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-19 21:28:58 +0100

    Update Color.java
    Se crea el método toString que devolverá la representación en forma de cadena del literal

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
@@ -9,4 +9,9 @@ public enum Color {
     private Color(String cadenaAMostrar) {
         this.cadenaAMostrar = cadenaAMostrar;
     }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
 }
