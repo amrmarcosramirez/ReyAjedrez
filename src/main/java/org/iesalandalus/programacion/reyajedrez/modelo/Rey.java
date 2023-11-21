20ae13fa4ebe095b02d6c6e92b9ef31d04e9ec4d
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-21 20:15:32 +0100

    Update Rey.java
    Se crean los métodos get y set para cada atributo con la visibilidad adecuada

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -1,8 +1,25 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import java.util.Objects;
+
 public class Rey {
     private int totalMovimientos;
     private Color color;
     private Posicion posicion;
 
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = Objects.requireNonNull(color, "El color del Rey no puede ser nulo.");
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = Objects.requireNonNull(posicion, "La posición del Rey no puede ser nula.");
+    }
 }
