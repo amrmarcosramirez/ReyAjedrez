804303262d4429dc61df81970c0fc0c658b4b9f3
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-21 21:21:54 +0100

    Update Rey.java
    Se cambia el parámetro totalMovimientos a (private static int) y se inicializa a cero. Además, se crea un constructor para la clase Rey que acepte como parámetro el color

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -3,12 +3,25 @@ package org.iesalandalus.programacion.reyajedrez.modelo;
 import java.util.Objects;
 
 public class Rey {
-    private int totalMovimientos;
+    private static int totalMovimientos = 0;
     private Color color;
     private Posicion posicion;
 
     public Rey(){
+        color = Color.BLANCO;
         posicion = new Posicion(1, 'e');
+    }
+
+    public Rey(Color color){
+        this();
+        Objects.requireNonNull(color, "El color no puede ser nulo.");
+        if(color == Color.BLANCO){
+            posicion = new Posicion(1, 'e');
+        } else if (color == Color.NEGRO) {
+            posicion = new Posicion(8, 'e');
+        }else {
+            throw new IllegalArgumentException("El color no es permitido.");
+        }
     }
 
     public Color getColor() {
