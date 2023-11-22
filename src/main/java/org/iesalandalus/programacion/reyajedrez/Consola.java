677a942293a08218895344607e1e7f88433e63fa
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-23 00:03:23 +0100

    Update Consola.java
    Se crea el constructor Consola.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -1,5 +1,10 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Color;
+import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
+
 public class Consola {
+    private Consola(){}
+
     
 }
