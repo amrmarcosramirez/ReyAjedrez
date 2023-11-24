8beeadeb5c80881b0b5abe5fbd3cda650d884c84
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-24 21:43:29 +0100

    Update Consola.java
    Se crea el método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -4,7 +4,14 @@ import org.iesalandalus.programacion.reyajedrez.modelo.Color;
 import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
 
 public class Consola {
+
     private Consola(){}
 
+    public static void mostrarMenu() {
+        System.out.println("1. Crear rey por defecto");
+        System.out.println("2. Crear rey eligiendo el color");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir");
+    }
     
 }
