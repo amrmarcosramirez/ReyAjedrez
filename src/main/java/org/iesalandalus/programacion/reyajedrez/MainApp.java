e7765ab4c0e5961bdf9c4bd1ae4d1f0bf7de3106
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-24 23:55:48 +0100

    Update MainApp.java
    Se crea el atributo de clase rey.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,12 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+
 public class MainApp {
+
     public static void main(String[] args) {
+        Rey rey;
+        
 
     }
 }
