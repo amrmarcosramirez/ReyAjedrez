73ed86dbc240c93c95cf4c26faeb0747bb7d8055
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-25 20:30:58 +0100

    Update MainApp.java
    Se crea el método crearReyColor

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,9 +1,11 @@
 package org.iesalandalus.programacion.reyajedrez;
 
 import org.iesalandalus.programacion.reyajedrez.modelo.Color;
+import org.iesalandalus.programacion.reyajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.reyajedrez.modelo.Posicion;
 import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
 
-import static org.iesalandalus.programacion.reyajedrez.Consola.elegirColor;
+import static org.iesalandalus.programacion.reyajedrez.Consola.*;
 
 public class MainApp {
     private Rey rey;
@@ -16,16 +18,11 @@ public class MainApp {
     private static void crearReyDefecto() {
         Rey rey = new Rey();
     }
-    
-    private static void ejecutarOpcion (int opcion) {
-        if (opcion == 1){
-            crearReyDefecto();
-        } else if (opcion == 2) {
-            crearReyColor();
-        } else if (opcion == 3) {
-            mover();
-        } else {
-            mostrarRey();
-        }
+
+    private static void crearReyColor() {
+        Color color = Consola.elegirColor();
+        Rey rey = new Rey(color);
     }
+
+    
 }
