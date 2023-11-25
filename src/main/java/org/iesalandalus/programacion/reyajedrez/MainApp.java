3faae657db37d5f8699ffd9f4f986d2fc775a819
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-11-25 01:30:29 +0100

    Update MainApp.java
    Se crea el método ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,6 +1,9 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Color;
 import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+
+import static org.iesalandalus.programacion.reyajedrez.Consola.elegirColor;
 
 public class MainApp {
     private Rey rey;
@@ -10,4 +13,15 @@ public class MainApp {
 
     }
 
+    private static void ejecutarOpcion (int opcion) {
+        if (opcion == 1){
+            crearReyDefecto();
+        } else if (opcion == 2) {
+            crearReyColor();
+        } else if (opcion == 3) {
+            mover();
+        } else {
+            mostrarRey();
+        }
+    }
 }
